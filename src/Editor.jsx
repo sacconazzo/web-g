1a7f0c84b39a226e7c7688a35150faf9371860ca
Giona Righini <grighini@alteanet.it>
2023-02-03 14:55:56 +0100

    check email ref.

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,14 +1,44 @@
 import React, { useRef } from 'react';
 import JoditEditor from "jodit-react";
+import { Jodit } from 'jodit';
+
+function checkMessage(editor) {
+    const statusbar = document.createElement('div');
+    statusbar.style.backgroundColor = '#f8f8f8';
+    statusbar.style.fontSize = '11px';
+    // statusbar.style.color = '#DC143C';
+    statusbar.style.padding = '1px 4px';
+    // statusbar.style.borderTop = '1px solid #dadada';
+
+    function calcStat() {
+        const text = Jodit.modules.Helpers.trim(editor.editor.innerText)
+        const emailsArray = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+        statusbar.innerText = emailsArray != null && emailsArray.length ? '' : 'your email reference is currently missing';
+    }
+
+    editor.events
+        .on('change afterInit', editor.async.debounce(calcStat, 100))
+        .on('afterInit', function () {
+            editor.container.appendChild(statusbar);
+        });
+}
 
 const Editor = (props) => {
-    const editor = useRef(null)
+    const editor = useRef(Jodit)
     //const [content, setContent] = useState('')
 
     const config = {
         readonly: false, // all options from https://xdsoft.net/jodit/doc/
-        toolbar: false
+        toolbar: false,
+        showCharsCounter: false,
+        showWordsCounter: false,
+        showXPathInStatusbar: false,
+        showPlaceholder: false,
+        hidePoweredByJodit: true,
+        statusbar: true
     }
+
+    Jodit.plugins.add('status', checkMessage);
 
     return (
         <JoditEditor
